53d79643e1201da046cb57e1e2bc9c3eaf9f9b59
mXispitlk <P3tr.juricek@gmail.com>
2020-02-11 22:09:43 +0000

    Setup React Object

--- Fullstack project spring react redux/src/main/java/cz/mxispitlk/ppmtool/repositories/ProjectRepository.java
+++ Fullstack project spring react redux/src/main/java/cz/mxispitlk/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,17 @@
 package cz.mxispitlk.ppmtool.repositories;
 
-public interface ProjectRepository {
+import cz.mxispitlk.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAll();
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+
+    Project findByProjectIdentifier(String projectId);
 }
